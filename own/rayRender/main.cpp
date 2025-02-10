08eb51a97c68c94a51951c6844782c0ca409280c
VanityNie <1162435706@qq.com>
2025-02-10 23:50:35 +0800

    cmake change

---
+++ own/rayRender/main.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int CALLBACK WinMain(
+        HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR lpCmdLine,
+        int nCmdShow
+) {
+
+
+    // 你的 GUI 应用程序初始化代码
+    return 0;
+}
